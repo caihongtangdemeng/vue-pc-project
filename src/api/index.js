720c4e29561a741e00e1f03987be6dcc855537cc
zhuyan <1414412625@qq.com>
2020-04-15 14:54:14 +0800

    updata

--- src/api/index.js
+++ src/api/index.js
@@ -3,7 +3,6 @@ import ajax from './ajax'
 import mockAjax from './mockAjax'
 
 export const reqBaseCategoryList=()=>ajax('/product/getBaseCategoryList') //首页三级分类get请求
-export const reqLogin=(mobile,password)=>ajax.post('/user/passport/login',{mobile,password}) //登录post请求
 
 export const reqBanners=()=>mockAjax.get('/banners')
 export const reqFloors=()=>mockAjax.get('/floors')
@@ -11,12 +10,15 @@ export const reqFloors=()=>mockAjax.get('/floors')
 export const reqProductList = (searchParams) => ajax.post('/list',searchParams)
 export const reqDetailInfo=(skuId)=>ajax.get(`/item/${skuId}`)
 // reqBanners().then(result=>{
-//   console.log('result',result)
-// })
-
+  //   console.log('result',result)
+  // })
+  
 export const reqAddToCart=(skuId,skuNum)=>ajax.post(`/cart/addToCart/${skuId}/${skuNum}`)
 
 export const reqCartList=()=>ajax.get('/cart/cartList')
 
 export const reqCheckCartItem=(skuId,isChecked)=>ajax.get(`/cart/checkCart/${skuId}/${isChecked}`)
 export const reqDeleteCartItem=(skuId)=>ajax.delete(`/cart/deleteCart/${skuId}`)
+export const reqLogin=(mobile,password)=>ajax.post('/user/passport/login',{mobile,password}) //登录post请求
+export const reqRegister=(userInfo)=>ajax.post('/user/passport/register',userInfo)
+export const reqLogout=()=>ajax.get('/user/passport/logout')
--- src/store/modules/user.js
+++ src/store/modules/user.js
@@ -1,11 +1,46 @@
 import {getUUID} from '@/utils/storageUtils'
+import {reqLogin,reqRegister,reqLogout} from '@/api'
+
 
 const state={
   userInfo:{},
   userTempId:getUUID()
 }
-const mutations={}
-const actions={}
+const mutations={
+RECEIVE_USER_INFO(state,userInfo){
+  state.userInfo=userInfo
+},
+RESET_USER_INFO(state){
+  state.userInfo={}
+}
+}
+const actions={
+  async login({commit},{mobile,password}){
+    const result =await reqLogin(mobile,password)
+    if(result.code===200){
+      const userInfo=result.data
+      commit('RECEIVE_USER_INFO',userInfo)
+    }else{
+      throw new Error(result.message||'登录失败');
+    }
+  },
+
+  async register({commit},userInfo){
+    const result=await reqRegister(userInfo)
+    if(result.code!==200){
+      throw new Error(result.message||'注册失败')
+    }
+  },
+
+  async logout ({commit}){
+    const result =await reqLogout()
+    if(result.code!==200){
+      throw new Error(result.message||'退出登录失败')
+    }else{
+      commit('RESET_USER_INFO')
+    }
+  }
+}
 const getters={}
 export default{
   state,
